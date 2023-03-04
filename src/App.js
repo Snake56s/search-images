8d49df374fe6851ba9d88ff9f94de6111bb5f45a
Snake56s <damianjerezmillanao@gmail.com>
2023-03-03 22:11:47 -0300

    modificacion

--- src/App.js
+++ src/App.js
@@ -1,72 +1,81 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Buscador from './componentes/Buscador';
 import Resultado from './componentes/Resultado';
-class App extends Component{
-  state={
-    termino : '',
-    imagenes : [],
-    pagina : ''
-  }
-  scroll = () =>{
+class App extends Component {
+  state = {
+    termino: '',
+    imagenes: [],
+    pagina: '',
+  };
+  scroll = () => {
     const elemento = document.querySelector('.jumbotron');
-    elemento.scrollIntoView('smooth','start')
-  }  
+    elemento.scrollIntoView('smooth', 'start');
+  };
   paginaAnterior = () => {
     let pagina = this.state.pagina;
-    if(pagina===1) return alert('No se puede seguir retrocediendo');
-    pagina -=1;
-    this.setState({
-      pagina: pagina
-    }, ()=>{
-      this.consultarApi();
-      this.scroll();
-    });
-  }
+    if (pagina === 1) return alert('No se puede seguir retrocediendo');
+    pagina -= 1;
+    this.setState(
+      {
+        pagina: pagina,
+      },
+      () => {
+        this.consultarApi();
+        this.scroll();
+      }
+    );
+  };
   paginaSiguiente = () => {
     let pagina = this.state.pagina;
-    pagina +=1;
-    this.setState({
-      pagina: pagina
-    }, ()=>{
-      this.consultarApi();
-      this.scroll();
-    });
-  }
+    pagina += 1;
+    this.setState(
+      {
+        pagina: pagina,
+      },
+      () => {
+        this.consultarApi();
+        this.scroll();
+      }
+    );
+  };
 
-  consultarApi = () =>{
+  consultarApi = () => {
     let termino = this.state.termino;
-    let pagina  = this.state.pagina;
-    let APIkey = 'your API KEY';
-    const url=`https://pixabay.com/api/?key=${APIkey}&q=${termino}&per_page=30&page=${pagina}`;
+    let pagina = this.state.pagina;
+    let APIkey = process.env.REACT_APP_API_KEY;
+    const url = `https://pixabay.com/api/?key=${APIkey}&q=${termino}&per_page=30&page=${pagina}`;
     fetch(url)
-    .then(respuesta=>respuesta.json())
-    .then(resultado=>this.setState({imagenes: resultado.hits}));
-  }
-  datoBusqueda = (termino) =>{
-    this.setState({
-      termino:termino,
-      pagina: 1
-    }, () => {
-      this.consultarApi();
-    });
-  }
-  
-  render(){
+      .then((respuesta) => respuesta.json())
+      .then((resultado) => this.setState({ imagenes: resultado.hits }));
+  };
+  datoBusqueda = (termino) => {
+    this.setState(
+      {
+        termino: termino,
+        pagina: 1,
+      },
+      () => {
+        this.consultarApi();
+      }
+    );
+  };
+
+  render() {
     return (
       <div className="container">
-        <div className='jumbotron'>
-          <h1 className='titulo text-center text-light'>Buscador de imagenes</h1>
-          <Buscador 
-            datoBusqueda = {this.datoBusqueda}
-            />
+        <div className="jumbotron">
+          <h1 className="titulo text-center text-light">
+            Buscador de imagenes
+          </h1>
+          <Buscador datoBusqueda={this.datoBusqueda} />
         </div>
-        <div className='row justify-content-center text-center'>
-          <Resultado 
-            Imagenes = {this.state.imagenes}
-            paginaAnterior = {this.paginaAnterior}
-            paginaSiguiente = {this.paginaSiguiente}
-            />
+        <div className="row justify-content-center text-center">
+          <Resultado
+            Imagenes={this.state.imagenes}
+            paginaAnterior={this.paginaAnterior}
+            paginaSiguiente={this.paginaSiguiente}
+          />
         </div>
       </div>
     );
--- src/componentes/Buscador.jsx
+++ src/componentes/Buscador.jsx
@@ -1,25 +1,33 @@
-import React,{Component} from "react";
-class Buscador extends Component{
-    
-    busquedaRef = React.createRef();
-    obtenerDatos = (e) =>{
-        e.preventDefault();
-        const termino = this.busquedaRef.current.value;
-        this.props.datoBusqueda(termino);
-    }
-    render(){
-        return( 
-            <form onSubmit={this.obtenerDatos}>
-                <div className="row">
-                    <div className="form-group col-md-9">
-                        <input ref={this.busquedaRef} type="text" className="form-control form-control-lg" placeholder="Buscar una imagen por Ejemplo Café"/>
-                    </div>
-                    <div className="form-group col-md-3">
-                        <input type="submit" className="btn btn-lg btn-danger btn-block" value='Buscar...'/>
-                    </div>
-                </div>
-            </form>
-        );
-    }
+import React, { Component } from 'react';
+class Buscador extends Component {
+  busquedaRef = React.createRef();
+  obtenerDatos = (e) => {
+    e.preventDefault();
+    const termino = this.busquedaRef.current.value;
+    this.props.datoBusqueda(termino);
+  };
+  render() {
+    return (
+      <form onSubmit={this.obtenerDatos}>
+        <div className="row">
+          <div className="form-group col-md-9">
+            <input
+              ref={this.busquedaRef}
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Buscar una imagen por Ejemplo Café"
+            />
+          </div>
+          <div className="form-group col-md-3">
+            <input
+              type="submit"
+              className="btn btn-lg btn-danger btn-block"
+              value="Buscar..."
+            />
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
 export default Buscador;
